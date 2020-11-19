Atrate/LibgenCheck

commit 30b1b84ac88e2d78fc8cadd7e25969e7e4aab379

[libgencheck] Explain the async dillema in code comments
Signed-off-by: Atrate <Atrate@protonmail.com>

src/libgencheck.js
@@ -92,9 +92,13 @@ function DEBUG() { VERBOSE_LEVEL >= 2 && console.log.apply(console, arguments); 
 INFO(argv.n ? `Files chosen for processing: ${argv._}`: `Files chosen for processing: `.bold + `${argv._}`.italic );
 INFO("");
 
-// Iterate through each argument from the argv._ and check files from there
-// ------------------------------------------------------------------------
-argv._.forEach(async file => 
+// Iterate through each argument from the argv._ and check files from there.
+// This will be made synchronous when:
+// a) The libgen module implements synchronous search
+// b) With the release of Webpack v5, which allows for await in sync functions.
+// See: https://stackoverflow.com/a/41957152
+// ----------------------------------------------------------------------------
+argv._.forEach(file => 
     {
         // Declare variables before try-catch blocks since JS is illogical and try-catches are scoped
         // ------------------------------------------------------------------------------------------

commit 8d7d09d02182322721d1e40823ea9cb4c19a51b4

[libgencheck] Fix regression introduced by 30b1b84
Signed-off-by: Atrate <Atrate@protonmail.com>

src/libgencheck.js
@@ -98,7 +98,7 @@ INFO("");
 // b) Webpack v5 is released, which would allow using await in sync functions.
 // See: https://stackoverflow.com/a/41957152
 // ---------------------------------------------------------------------------
-argv._.forEach(file => 
+argv._.forEach(async file => 
     {
         // Declare variables before try-catch blocks since JS is illogical and try-catches are scoped
         // ------------------------------------------------------------------------------------------